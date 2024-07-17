9d152b850017b6cbe9814e35d7d8efe8a82ffae0
deepbuzin <andrew@tensorsense.com>
2024-07-17 04:53:06 +0000

    Reorder imports to avoid looping

--- training_toolkit/__init__.py
+++ training_toolkit/__init__.py
@@ -1,3 +1,5 @@
+from .src.common.video_readers import get_video_reader
+
 from .src.train_builder import build_trainer
 
 from .src.importers.qa import ImageQAImporter, VideoQAImporter
@@ -11,6 +13,5 @@ from .src.model_presets.video import llava_next_video_preset
 from .src.data_presets.qa import image_qa_preset, video_qa_preset
 from .src.data_presets.json import image_json_preset
 
-from .src.common.video_readers import get_video_reader
 from .src.utils.visualization import animate_video_sample
 from .src.utils.inference import process_raw_video
