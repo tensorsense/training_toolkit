546a85061d0e8e7790197146528c204091f5e1fc
deepbuzin <andrew@tensorsense.com>
2024-08-20 15:23:36 +0000

    Add video JSON preset

--- training_toolkit/__init__.py
+++ training_toolkit/__init__.py
@@ -7,6 +7,6 @@ from .core.types import DataPreset, ModelPreset
 from .model_presets.image import paligemma_image_preset
 from .model_presets.video import llava_next_video_preset
 from .data_presets.qa import image_qa_preset, video_qa_preset
-from .data_presets.json import image_json_preset
+from .data_presets.json import image_json_preset, video_json_preset
 from .data_presets.segmentation import image_segmentation_preset
 from .utils.visualization import animate_video_sample
--- training_toolkit/data_presets/json.py
+++ training_toolkit/data_presets/json.py
@@ -1,5 +1,8 @@
 from training_toolkit import DataPreset
 from training_toolkit.common.tokenization_utils.json import JSONTokenizer
+from training_toolkit.common.video_readers import get_video_reader
+import os
+
 
 import torch
 import json
@@ -53,4 +56,80 @@ class ImageJSONCollatorWithPadding:
 image_json_preset = DataPreset(
     train_test_split=0.2,
     collator_cls=ImageJSONCollatorWithPadding,
+)
+
+
+class VideoJSONCollator:
+    def __init__(self, processor, num_frames=8, max_length=256):
+        self.processor = processor
+
+        self.num_frames = num_frames
+        self.max_length = max_length
+
+        self.num_proc = os.cpu_count()
+        self.read_video_fn = get_video_reader()
+
+    def __call__(self, examples):
+        samples = []
+        for example in examples:
+
+            video = torch.tensor(
+                self.read_video_fn(
+                    example["video"],
+                    self.num_frames,
+                )
+            )
+
+            conversation = [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "extract JSON."},
+                        {"type": "video"},
+                    ],
+                },
+                {
+                    "role": "assistant",
+                    "content": [
+                        {"type": "text", "text": json.dumps(example["json"])},
+                    ],
+                },
+            ]
+
+            prompt = self.processor.apply_chat_template(
+                conversation, add_generation_prompt=False
+            )
+
+            sample = self.processor(
+                text=prompt,
+                videos=video,
+                truncation=True,
+                max_length=self.max_length,
+                return_tensors="pt",
+            )
+            samples.append(sample)
+
+        padded_inputs = self.processor.tokenizer.pad(
+            {
+                "input_ids": [
+                    sample["input_ids"][0] for sample in samples
+                ],  # each element is one batch only so we slice [0]
+                "attention_mask": [sample["attention_mask"][0] for sample in samples],
+            },
+            padding=True,
+            return_tensors="pt",
+        )
+
+        labels = padded_inputs["input_ids"].clone()
+        labels[labels == self.processor.tokenizer.pad_token_id] = -100
+        padded_inputs["labels"] = labels
+        padded_inputs["pixel_values_videos"] = torch.cat(
+            [sample["pixel_values_videos"] for sample in samples], dim=0
+        )
+        return padded_inputs
+
+
+video_json_preset = DataPreset(
+    train_test_split=0.2,
+    collator_cls=VideoJSONCollator,
 )
